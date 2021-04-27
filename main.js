c5636eb259a41939b6f3c75bc5e576965a0191ec
savjaylade84@gmail.com <John Jayson b de leon>
2021-04-28 04:22:12 +0800

    playable music player | problem is it doesnt not work on seperate json file

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 // get the important element
 const music_container = document.querySelector('.music-container');
 const play_btn = document.querySelector('#play');
@@ -10,49 +11,201 @@ const progress_container = document.querySelector('.progress-container');
 const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
-let song_list;
+let song_list = ` [
+   {
+      "title" : "Somewhere Now", 
+      "url" : "./music/revolutionary_radio/01_Somewhere_Now.mp3", 
+      "cover" : "./images/revolution.jpg"
+    },
+    {
+       "title": "Bang Bang",
+       "url": "./music/revolutionary_radio/02_Bang_Bang.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Revolution Radio",
+       "url": "./music/revolutionary_radio/03_Revolution_Radio.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Say Goodbye",
+       "url": "./music/revolutionary_radio/04_Say_Goodbye.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Outlaws",
+       "url": "./music/revolutionary_radio/05_Outlaws.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Bouncing Off The Wall",
+       "url": "./music/revolutionary_radio/06_Bouncing_Off_The_Wall.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Still Breathing",
+       "url": "./music/revolutionary_radio/07_Still_Breathing.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Youngblood",
+       "url": "./music/revolutionary_radio/08_Youngblood.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Too Dumb To Die",
+       "url": "./music/revolutionary_radio/09_Too_Dumb_To_Die.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Troubled Time",
+       "url": "./music/revolutionary_radio/10_Troubled_Time.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Forever Now",
+       "url": "./music/revolutionary_radio/11_Forever_Now.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "Forever Now",
+       "url": "./music/revolutionary_radio/11_Forever_Now.mp3",
+       "cover": "./images/revolution.jpg"
+     },
+     {
+       "title": "2000 Light Years Away",
+       "url": "./music/god_greatest_band/01_2000_Light_Years_Away.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "2000 Light Years Away",
+       "url": "./music/god_greatest_band/01_2000_Light_Years_Away.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Longview",
+       "url": "./music/god_greatest_band/02_Longview.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Welcome To Paradise",
+       "url": "./music/god_greatest_band/03_Welcome_To_Paradise.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Basket Case",
+       "url": "./music/god_greatest_band/04_Basket_Case.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "When I Come Around",
+       "url": "./music/god_greatest_band/05_When_I_Come_Around.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "She",
+       "url": "./music/god_greatest_band/06_She.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Brain Stew",
+       "url": "./music/god_greatest_band/07_Brain_Stew.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Hitchin' A Ride",
+       "url": "./music/god_greatest_band/08_Hitchin_A_Ride.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Good Riddance",
+       "url": "./music/god_greatest_band/09_Good_Riddance.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Minority",
+       "url": "./music/god_greatest_band/10_Minority.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Warning",
+       "url": "./music/god_greatest_band/11_Warning.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "American Idiot",
+       "url": "./music/god_greatest_band/12_American_Idiot.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Holiday",
+       "url": "./music/god_greatest_band/13_Holiday.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Boulevard Of Broken Dreams",
+       "url": "./music/god_greatest_band/14_Boulevard_Of_Broken_Dreams.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Wake Me Up When September Ends",
+       "url": "./music/god_greatest_band/15_Wake_Me_Up_When_September_Ends.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Know Your Enemy",
+       "url": "./music/god_greatest_band/16_Know_Your_Enemy.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "21 Guns",
+       "url": "./music/god_greatest_band/17_21_Guns.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Oh Love",
+       "url": "./music/god_greatest_band/18_Oh_Love.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Bang Bang",
+       "url": "./music/god_greatest_band/19_Bang_Bang.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Still Breathing",
+       "url": "./music/god_greatest_band/20_Still_Breathing.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Ordinary World",
+       "url": "./music/god_greatest_band/21_Ordinary_World.mp3",
+       "cover": "./images/greatest_band.jpg"
+     },
+     {
+       "title": "Back In The USA",
+       "url": "./music/god_greatest_band/22_Back_In_The_USA.mp3",
+       "cover": "./images/greatest_band.jpg"
+     }
+  ]
+`;
 let song_index;
+
+//set and load song json 
+ song_list = JSON.parse(song_list);
+ song_index = 0;
+ load_song(song_list[song_index]);
 
   //update song
  function load_song(song) {
 
-  title.innerHTML = `${String(song.title).toString()}`;
-  audio.src = `${String(song.url).toString()}`;
-  cover.href = `${String(song.cover).toString()}`;
+  title.innerHTML =`${song.title}`;
+  audio.src = `${song.url}`;
+  cover.src = `${song.cover}`;
       console.log(song);
 
   };
 
-//get and load song list from json file
-function loadJson(){
-  
-  fetch("songs.json").
-  then(res =>{
-  
-    if(res.ok)
-     JSON.parse(res.json());
-    
-  }).then(data => {
-    
-    console.log(data);
-    
-    //set and store song json file
-    song_list = data;
-    
-    //track songs list count
-    song_index = song_list.length;
-    
-    //load the song
-    load_song(song_list[song_index]);
-    console.log("song is loaded");
-
-  }).catch(error => {
-    console.log("json parsing failed");
-  });
-  
-} 
-
-window.addEventListener('load',loadJson);
 
 // play the song
 function play_song(){
